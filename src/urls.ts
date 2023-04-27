37e09e7b7cfd5c1323d4739499493f47ddccbee9
melewasy <eng.m.elewasy@gmail.com>
2023-04-27 14:15:00 +0200

    remove temp files

--- src/urls.ts
+++
@@ -1,4 +0,0 @@
-export const linkedinURL =
-  "https://www.linkedin.com/in/mohamed-mostafa-el-ewasy/";
-export const githubURL = "https://github.com/mohamedelewasy";
-export const facebookURL = "https://www.facebook.com/eng.mohamed.elewasy/";
