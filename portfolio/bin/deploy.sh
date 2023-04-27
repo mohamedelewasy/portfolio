112df1eceeeeef4586e2a79e43bedd4237ecbed9
melewasy <eng.m.elewasy@gmail.com>
2023-04-27 15:21:33 +0200

    remove circleci

--- portfolio/bin/deploy.sh
+++
@@ -1,2 +0,0 @@
-aws s3 cp --recursive --acl public-read ./build s3://portfolio106191034073/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./build/index.html s3://portfolio106191034073/
